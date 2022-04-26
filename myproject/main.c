cf2b8d7ff6a425e5ede5edd1e6c62beee063048f
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-04-25 21:18:42 -0300

    feat: add main, CMakeLists and build directory

---
+++ myproject/main.c
@@ -0,0 +1,19 @@
+/**
+ * main.c generico para nuevo proyecto.
+ *
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+
+    /* Inicializa el standard input/output de la pico */
+    stdio_init_all();
+    
+    while (true) {
+    
+        /* Bucle infinito */
+    }
+    return 0;
+}
