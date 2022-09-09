fbac812319db6313057f598f419f8766cd12c739
Fabrizio <fabrizio.carlassara@gmail.com>
2022-09-09 16:22:07 -0300

    init function and better config struct

--- pico-examples/spi/mfrc522/lib/inc/mfrc522.h
+++ pico-examples/spi/mfrc522/lib/inc/mfrc522.h
@@ -3,15 +3,18 @@
 
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include "mfrc522_reg.h"
 
 #define SPI0_DEFAULT_MISO   16
 #define SPI0_DEFAULT_SS     17
 #define SPI0_DEFAULT_SCK    18
 #define SPI0_DEFAULT_MOSI   19
 
-#define MFRC_FREQUENCY      500000       
+#define MFRC_DEFAULT_FREQUENCY      500000       
 
 typedef struct {
+    spi_inst_t *spi;
+    uint32_t freq;
     uint8_t mosi;
     uint8_t miso;
     uint8_t sck;
@@ -19,10 +22,12 @@ typedef struct {
     uint8_t rst;
 } mfrc_config_t;
 
-void mfrc522_config_init(mfrc_config_t config);
+void mfrc522_config_init(mfrc_config_t *config);
 
 static inline mfrc_config_t mfrc522_get_default_config(void) {
-    return (mfrc_config_t) { 
+    return (mfrc_config_t) {
+        spi0,
+        MFRC_DEFAULT_FREQUENCY,
         SPI0_DEFAULT_MOSI, 
         SPI0_DEFAULT_MISO, 
         SPI0_DEFAULT_SCK, 
--- pico-examples/spi/mfrc522/lib/mfrc522.c
+++ pico-examples/spi/mfrc522/lib/mfrc522.c
@@ -1,9 +1,23 @@
 #include "mfrc522.h"
 
-void mfrc522_config_init(mfrc_config_t config) {
+static mfrc_config_t *mfrc;
 
-    spi_init(spi0, MFRC_FREQUENCY);
-    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+void mfrc522_config_init(mfrc_config_t *config) {
+    /* Save configuration struct */
+    mfrc = config;
+    /* Initialize SPI */
+    spi_init(config->spi, config->freq);
+    /* Configure MOSI, MISO and SCK pins */
+    gpio_set_function(config->mosi, GPIO_FUNC_SPI);
+    gpio_set_function(config->miso, GPIO_FUNC_SPI);
+    gpio_set_function(config->sck, GPIO_FUNC_SPI);
+    /* Configure SS as output */
+    gpio_init(config->ss);
+    gpio_set_dir(config->ss, true);
+    gpio_put(config->ss, true);
+    /* Configure RST pin, if available */
+    if(config->rst > -1) { 
+        gpio_init(config->rst);
+        gpio_set_dir(config->rst, true); 
+    }
 }
