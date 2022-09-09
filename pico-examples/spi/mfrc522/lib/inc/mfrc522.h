c73fea3b22bd669af1620562313ae0e517214243
Fabrizio <fabrizio.carlassara@gmail.com>
2022-09-09 15:28:22 -0300

    add config struct and init function

--- pico-examples/spi/mfrc522/lib/inc/mfrc522.h
+++ pico-examples/spi/mfrc522/lib/inc/mfrc522.h
@@ -1,6 +1,34 @@
 #ifndef MFRC522_H_
 #define MFRC522_H
 
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
 
+#define SPI0_DEFAULT_MISO   16
+#define SPI0_DEFAULT_SS     17
+#define SPI0_DEFAULT_SCK    18
+#define SPI0_DEFAULT_MOSI   19
+
+#define MFRC_FREQUENCY      500000       
+
+typedef struct {
+    uint8_t mosi;
+    uint8_t miso;
+    uint8_t sck;
+    uint8_t ss;
+    uint8_t rst;
+} mfrc_config_t;
+
+void mfrc522_config_init(mfrc_config_t config);
+
+static inline mfrc_config_t mfrc522_get_default_config(void) {
+    return (mfrc_config_t) { 
+        SPI0_DEFAULT_MOSI, 
+        SPI0_DEFAULT_MISO, 
+        SPI0_DEFAULT_SCK, 
+        SPI0_DEFAULT_SS, 
+        -1 
+    };
+}
 
 #endif  /* MFRC522_H */
--- pico-examples/spi/mfrc522/lib/mfrc522.c
+++ pico-examples/spi/mfrc522/lib/mfrc522.c
@@ -1,1 +1,9 @@
 #include "mfrc522.h"
+
+void mfrc522_config_init(mfrc_config_t config) {
+
+    spi_init(spi0, MFRC_FREQUENCY);
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+}
