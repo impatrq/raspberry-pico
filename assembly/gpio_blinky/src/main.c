ef104585d247b7183b4ffb7f76b144805b5219fb
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2023-12-07 08:53:48 -0300

    renombro funciones y agrego delay de 500 ms

--- assembly/gpio_blinky/src/main.c
+++ assembly/gpio_blinky/src/main.c
@@ -1,12 +1,18 @@
 #include "pico/stdlib.h"
 
 // Funciones externas de assembler
-extern void init();
-extern void blink();
+extern void asm_led_init();
+extern void asm_led_blink();
+extern void asm_delay();
 
 int main(void) {  
     // Llamo al init de assembler
-    init();
-    // Llamo al blink de assembler
-    blink();
+    asm_led_init();
+
+    while(true) {
+        // Llamo al blink de assembler
+        asm_led_blink();
+        // Llamo al delay de assembler 500 ms
+        asm_delay();
+    }
 }
